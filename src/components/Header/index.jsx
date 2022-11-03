cf39a35764a3f226bdd2f4de83b93dec4d9eeb39
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-11-02 21:56:17 -0500

    ⚡ [UPDATE] Header Cart

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,22 @@
+import { useAppContext } from '@/hooks/useAppContext';
 import { UilShoppingBag } from '@iconscout/react-unicons';
 import { Link } from "react-router-dom";
 import "./styles.css";
 
 export const Header = () => {
+  const { state } = useAppContext();
+
   return(
     <header className="header">
       <nav className="navbar container">
         <Link to="/">
           Platzi Conf Merch
         </Link>
-        <Link to="/checkout">
+        <Link to="/checkout" className="cart">
+          {!!state.cart.length &&
+          <p className="items-count">{state.cart.length}</p>
+          }
+
           <UilShoppingBag/>
         </Link>
       </nav>
